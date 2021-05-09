4e25cf78f0751a03f0fa982244aa225ac1746cf7
Maxime Rauer <maxime.rauer@gmail.com>
2021-05-08 19:21:21 -0500

    [MID] List of Subscriptions (#3)

--- lib/errors.go
+++ lib/errors.go
@@ -1,14 +1,14 @@
 package lib
 
 import (
-  "log"
+	"log"
 )
 
 func HandleError(err error, message string) {
-  if message == "" {
-    message = "Error making API call"
-  }
-  if err != nil {
-    log.Fatalf(message + ": %v", err.Error())
-  }
+	if message == "" {
+		message = "Error making API call"
+	}
+	if err != nil {
+		log.Fatalf(message+": %v", err.Error())
+	}
 }
